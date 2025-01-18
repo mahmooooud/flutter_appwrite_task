73bc7c4886bec29710a638f69e4c0b43736e3f6e
Mahmoud Ahmed <ebrahimahmed2246@gmail.com>
2025-01-18 18:13:23 +0200

    edits

--- lib/core/ui/show_error_widget.dart
+++ lib/core/ui/show_error_widget.dart
@@ -1,13 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_appwrite_task/core/error/bad_request_error.dart';
-import 'package:flutter_appwrite_task/core/error/cancel_error.dart';
-import 'package:flutter_appwrite_task/core/error/connection_error.dart';
-import 'package:flutter_appwrite_task/core/error/custom_error.dart';
-import 'package:flutter_appwrite_task/core/error/forbidden_error.dart';
-import 'package:flutter_appwrite_task/core/error/internal_server_error.dart';
-import 'package:flutter_appwrite_task/core/error/not_found_error.dart';
-import 'package:flutter_appwrite_task/core/error/timeout_error.dart';
-import 'package:flutter_appwrite_task/core/error/unauthorized_error.dart';
 import 'package:flutter_appwrite_task/core/ui/page_error_view.dart';
 import '../bloc/base_bloc/base_state.dart';
 
@@ -22,55 +14,7 @@ class ShowErrorWidget extends StatelessWidget {
       return Container();
     }, failure: (error, callback) {
       // Connection Error
-      if (error is ConnectionError) {
-        return PageErrorView(
-          errorMessage: "An error occurred during the connection",
-          action: callback,
-          actionName: 'TryAgain',
-        );
-      }
-      // Custom Error
-      else if (error is CustomError) {
-        return PageErrorView(
-          errorMessage: error.message,
-        );
-      }
-      // Unauthorized Error
-      else if (error is UnauthorizedError) {
-        return UnauthorizedErrorScreenWidget();
-      }
-      // Not Found Error
-      else if (error is NotFoundError) {
-        return PageErrorView(
-          errorMessage: error.message!,
-        );
-      }
-      // Bad Request Error
-      else if (error is BadRequestError) {
-        return PageErrorView(
-          errorMessage: error.message!,
-        );
-      }
-      // Forbidden Error
-      else if (error is ForbiddenError) {
-        return PageErrorView(
-          errorMessage: error.toString(),
-        );
-      }
-      // Internal Server Error
-      else if (error is InternalServerError) {
-        return PageErrorView(
-          action: callback,
-          actionName: 'TryAgain',
-          errorMessage: error.message!,
-        );
-      } else if (error is TimeoutError) {
-        return PageErrorView(
-          action: callback,
-          actionName: 'TryAgain',
-          errorMessage: error.toString(),
-        );
-      } else if (error is CancelError) {
+       if (error is BadRequestError) {
         return PageErrorView(
           action: callback,
           actionName: 'TryAgain',
